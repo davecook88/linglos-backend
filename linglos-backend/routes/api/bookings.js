9d058f285921269f00de19a8665e7a587a16284e
Dave Cook <DaveCook@hotmail.co.uk>
2020-04-19 09:53:01 +0700

    change booking API to async waterfall

--- linglos-backend/routes/api/bookings.js
+++ linglos-backend/routes/api/bookings.js
@@ -8,9 +8,9 @@ const Appointment = require('../../models/Appointment');
 // @desc Creates new appointment if it doesn't exist
 // @access Public
 router.post("/makeappointment", (req, res) => {
-  console.log(req.body);
   const { teacher, time, userId } = req.body;
-  const startTimeInMilliseconds = new Date(time.start).getTime();
+  const startDateObj = new Date(time.start);
+  const startTimeInMilliseconds = startDateObj.getTime();
   const teacherId = teacher._id;
 
   User.findOne({_id:userId}, (err, user) => {
@@ -18,45 +18,56 @@ router.post("/makeappointment", (req, res) => {
     let errors = {};
     let responseObj = {};
     const appointmentId = teacherId + startTimeInMilliseconds;
-
     Appointment.findOne({_id:appointmentId})
       .then((appointment) => {
         if (appointment) {
           const attendeeStudents = appointment.studentIds;
           attendeeStudents.push(user.id);
           appointment.studentIds = attendeeStudents;
-          newAppointment.save()
+          appointment.save()
             .then(appointment => responseObj.appointment = {message:`Appointment:  ${appointmentId} updated successfully`})
-            .catch(err => errors.appointment = err);
+            .catch(err => errors.appointment = [err.message, err.name, err.stack]);
         } else {
           const newAppointment = new Appointment({
             _id: appointmentId,
             teacherId:teacherId,
-            studentIds:[user._id],
+            studentIds:[user.id],
             time:time
           });
+          console.log(newAppointment);
           newAppointment.save()
             .then(appointment => responseObj.appointment = {message:`Appointment:  ${appointmentId} created successfully`})
-            .catch(err => errors.appointment = err);
+            .catch(err => errors.appointment = [err.message, err.name, err.stack]);
         }
         let studentAppointments = user.studentAppointments;
         studentAppointments.push(appointmentId);
         user.studentAppointments = studentAppointments;
         user.save()
           .then(user => responseObj.appointment = {message:`User:  ${user.id} updated successfully`})
-          .catch(err => errors.appointment = err);
-        User.findOne({id: teacherId}, (err, teacher) => {
-          if (err) errors.appointment = err;
-          let teacherAppointments = teacher.teacherAppointments;
-          teacherAppointments.push(appointmentId);
-          teacher.teacherAppointments = teacherAppointments;
-          teacher.save()
-            .then(user => responseObj.appointment = {message:`User (teacher):  ${user.id} updated successfully`})
-            .catch(err => errors.appointment = err);
+          .catch(err => errors.appointment = [err.message, err.name, err.stack]);
+        User.findOne({_id: teacherId}, (err, teacherRecord) => {
+          if (err) errors.appointment = [err.message, err.name, err.stack];
+          if (teacherRecord) {
+            let teacherAppointments = teacherRecord.teacherAppointments;
+            teacherAppointments.push(appointmentId);
+            teacherRecord.teacherAppointments = teacherAppointments;
+            teacherRecord.save()
+              .then(user => responseObj.appointment = {message:`User (teacher):  ${user.id} updated successfully`})
+              .catch(err => errors.appointment = [err.message, err.name, err.stack]);
+          } else {
+            errors.teacherRecord = {message:`User (teacher): ${teacherId} not found`};
+          }
+          
+          res.status(200).json({body:responseObj, errors:errors, });
         })
       })
-      .catch(err => res.status(200).json({body:responseObj, errors:errors, }));
-      res.status(200).json({body:responseObj, errors:errors, });
+      .catch(err => {
+        console.log(responseObj);
+        console.log(err);
+        errors.general = [err.message, err.name, err.stack];
+        res.status(200).json({body:responseObj, errors:errors, });
+      });
+      
   });
 })
 
